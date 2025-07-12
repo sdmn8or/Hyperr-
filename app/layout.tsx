711d5f3e4e931187236a9540fcc36c860977e8ed
Sadmaan Al Saad <sadmaan365@gmail.com>
2025-07-12 14:13:10 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Hyperr',
+  description: 'Im The best',
+  generator: 'sadmaan.com',
 }
 
 export default function RootLayout({
